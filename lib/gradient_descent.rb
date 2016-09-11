a216b4b9a86917a3820b486544a74f03a15e3632
agisga <alexej.go@googlemail.com>
2016-09-11 18:28:47 -0500

    quick&dirty gradient descent with and without backtracking line search; closely following lab1 of Eric Chi's summer school course: https://github.com/echi/samsi_opt.git

---
+++ lib/gradient_descent.rb
@@ -0,0 +1,8 @@
+# Copyright (c) 2016 Alexej Gossmann 
+
+require "gradient_descent/version"
+require "gradient_descent/gradient_descent"
+
+module GradientDescent
+  # Your code goes here...
+end
---
+++ lib/gradient_descent/version.rb
@@ -0,0 +1,3 @@
+module GradientDescent
+  VERSION = "0.1.0"
+end
